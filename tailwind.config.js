b073bb7c3f5fadfbeb735377d9ea1f987161c2bf
claupanca <55442866+claupanca@users.noreply.github.com>
2025-11-02 18:58:12 +0200

    another commit7

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add any other folders where Tailwind classes are used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
