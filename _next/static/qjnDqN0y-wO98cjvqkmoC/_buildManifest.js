689965867ee4d263b93e3b608d8f15ab88e307fa
claupanca <55442866+claupanca@users.noreply.github.com>
2025-11-02 18:32:13 +0200

    Updates

--- _next/static/qjnDqN0y-wO98cjvqkmoC/_buildManifest.js
+++ _next/static/qjnDqN0y-wO98cjvqkmoC/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/testMike//_next/:path+",
+        "destination": "/testMike/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
